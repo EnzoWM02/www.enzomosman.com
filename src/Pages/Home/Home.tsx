6a3d33e53584b3017ea11f8a99fd2b09b45b2623
EnzoWM02 <enzowm02@gmail.com>
2023-06-21 21:19:43 -0300

    #1 - First component Home - Removing original page

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import styles from './Home.module.css'
+
+export default function Home ({}) {
+
+    return (
+        <div className={`${styles.pageWrapper}`}>
+            <div className={`${styles.a}`}>
+            
+            </div>
+        </div>
+    )
+}

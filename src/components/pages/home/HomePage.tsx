e0465730bd67dc1b2f96a451321fc03d3bb599db
EnzoWM02 <enzowmosman@gmail.com>
2023-06-30 23:46:51 -0300

    #9 - Finished home page responsivity - Did some page layout tweaks related to reserve mobile navbar space in the bottom of page

--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -5,15 +5,16 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import photo from '@/assets/temp.png';
-import { Links } from '@/utils/Links.tsx';
 
 import { motion } from 'framer-motion';
 import { ExitAnimation } from '@/utils/Animations.tsx';
 import ShinyImage from '@/components/ui/ShinyImage.tsx';
 import SocialButtons from '@/components/ui/SocialButtons.tsx';
+import { useLayoutContext } from '@/hooks/useLayoutContext.tsx';
 
 export default function HomePage() {
   const { t } = useTranslation();
+  const { isLowRes, mobileBreakpoint } = useLayoutContext();
 
   const itemVariants = {
     hidden: { opacity: 0, y: 10 },
@@ -34,7 +35,7 @@ export default function HomePage() {
         variants={{ visible: { transition: { staggerChildren: 0.2 } } }}
         {...ExitAnimation}
       >
-        <div className={`${styles.titleContainer}`}>
+        <div className={`${styles.titleContainer} ${isLowRes && styles.lowRes} ${mobileBreakpoint && styles.mobile}`}>
           <motion.span variants={itemVariants} className={`title`}>
             {t('home.title1')}
           </motion.span>
@@ -44,22 +45,36 @@ export default function HomePage() {
           >
             {t('home.title2')}
           </motion.span>
-          <motion.span variants={itemVariants} className={`${styles.desc}`}>
-            {t('home.desc')}
-          </motion.span>
+          {mobileBreakpoint && (
+            <motion.div variants={itemVariants} className={`${styles.mobileImageContainer}`}>
+              <ShinyImage className={`${styles.mobileImage}`} imageSrc={photo} imageAlt={'enzo photo'} />
+            </motion.div>
+          )}
+          {!mobileBreakpoint && (
+            <motion.span variants={itemVariants} className={`${styles.desc}`}>
+              {t('home.desc')}
+            </motion.span>
+          )}
           <motion.div className={`${styles.socialButtonsContainer}`} variants={itemVariants}>
             <SocialButtons />
           </motion.div>
+          {mobileBreakpoint && (
+            <motion.span variants={itemVariants} className={`${styles.desc}`}>
+              {t('home.desc')}
+            </motion.span>
+          )}
         </div>
-        <motion.div
-          variants={imageVariant}
-          className={`${styles.imageContainer}`}
-        >
-          <motion.div className={`${styles.outerCircle}`} />
-          <motion.div className={`${styles.outerFullCircle}`} />
-          <motion.div className={`${styles.innerFullCircle}`} />
-          <ShinyImage className={`${styles.shinyImage}`} imageSrc={photo} imageAlt={'enzo photo'} />
-        </motion.div>
+        {!mobileBreakpoint && (
+          <motion.div
+            variants={imageVariant}
+            className={`${styles.imageContainer} ${isLowRes && styles.lowRes}`}
+          >
+            <motion.div className={`${styles.outerCircle}`} />
+            <motion.div className={`${styles.outerFullCircle}`} />
+            <motion.div className={`${styles.innerFullCircle}`} />
+            <ShinyImage className={`${styles.shinyImage}`} imageSrc={photo} imageAlt={'enzo photo'} />
+          </motion.div>
+        )}
       </motion.div>
     </React.Fragment>
   );
--- src/components/ui/navigation/PageLayout.tsx
+++ src/components/ui/navigation/PageLayout.tsx
@@ -1,11 +1,14 @@
 import styles from './PageLayout.module.css';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar.tsx';
+import { useLayoutContext } from '@/hooks/useLayoutContext.tsx';
 
 export default function PageLayout() {
 
+  const {mobileBreakpoint} = useLayoutContext();
+
   return (
-    <div className={`${styles.pageWrapper}`}>
+    <div className={`${styles.pageWrapper} ${mobileBreakpoint && styles.mobile}`}>
       <Navbar />
       <Outlet />
     </div>

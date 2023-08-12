2c6ce68f7d8dae286cb7e5a8332effa4e1244e02
EnzoWM02 <enzowmosman@gmail.com>
2023-08-12 19:16:41 -0300

    #24 - VSCode adjustments

--- src/components/pages/about/AboutPage.tsx
+++ src/components/pages/about/AboutPage.tsx
@@ -1,6 +1,5 @@
 import styles from './AboutPage.module.css';
 
-import React from 'react';
 import DefaultTabLayout from 'src/components/ui/navigation/DefaultTabLayout.tsx';
 import photo from 'src/assets/temp.png';
 import { useTranslation } from 'react-i18next';
@@ -9,18 +8,23 @@ import { motion } from 'framer-motion';
 import { itemVariants } from 'src/utils/Animations.tsx';
 
 export default function AboutPage() {
-
-  const {t} = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <DefaultTabLayout title={'about.title'}>
       <div className={`${styles.pageWrapper}`}>
         <div className={`${styles.pageContainer}`}>
           <RoundedImageShape />
-          <motion.div variants={itemVariants} className={`${styles.textContainer}`}>
-            <span className={`${styles.copy}`} dangerouslySetInnerHTML={{__html: t('about.copy')}} />
+          <motion.div
+            variants={itemVariants}
+            className={`${styles.textContainer}`}
+          >
+            <span
+              className={`${styles.copy}`}
+              dangerouslySetInnerHTML={{ __html: t('about.copy') }}
+            />
             <div className={`${styles.email}`}>
-              <i className="fa-solid fa-envelope"/>
+              <i className="fa-solid fa-envelope" />
               <span>{t('about.email')}</span>
             </div>
             <div className={`${styles.sideBar}`} />
@@ -34,7 +38,10 @@ export default function AboutPage() {
 
 function RoundedImageShape() {
   return (
-    <motion.div variants={itemVariants} className={`${styles.imageShapeContainer}`}>
+    <motion.div
+      variants={itemVariants}
+      className={`${styles.imageShapeContainer}`}
+    >
       <div className={`${styles.photoContainer}`}>
         <img className={`${styles.photo}`} src={photo} alt={'Enzo photo'} />
       </div>
